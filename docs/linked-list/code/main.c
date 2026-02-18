53161e3644f9fcda1e8fb92a7464bc79471324ef
XilefEel <felixlee175@gmail.com>
2026-02-18 21:57:53 +0700

    added c and rust source code for linked list

---
+++ docs/linked-list/code/main.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+
+Node *create_node(int data) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+Node *get_node(Node *head, int index) {
+    Node *current = head;
+    int i = 0;
+
+    while (current != NULL) {
+        if (i == index) {
+            return current;
+        }
+
+        current = current->next;
+        i++;
+    }
+
+    return NULL;
+}
+
+void print_list(Node *head) {
+    Node *current = head;
+
+    while (current != NULL) {
+        printf("%d", current->data);
+
+        if (current->next != NULL) {
+            printf(" -> ");
+        }
+
+        current = current->next;
+    }
+
+    printf("\n");
+}
+
+void traverse_apply(Node *head, void (*func)(int *)) {
+    Node *current = head;
+
+    while (current != NULL) {
+        func(&current->data);
+
+        current = current->next;
+    }
+}
+
+void double_value(int *data) {
+    *data *= 2;
+}
+
+void insert_at_head(Node **head, int data) {
+    Node *new_node = create_node(data);
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void insert_at_tail(Node **head, int data) {
+    Node *new_node = create_node(data);
+
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
+
+    Node *current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = new_node;
+}
+
+void insert_at_index(Node **head, int data, int index) {
+    if (index == 0) {
+        insert_at_head(head, data);
+        return;
+    }
+
+    Node *current = *head;
+    for (int i = 0; i < index - 1 && current != NULL; i++) {
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Index out of bounds\n");
+        return;
+    }
+
+    Node *new_node = create_node(data);
+
+    new_node->next = current->next;
+    current->next = new_node;
+}
+
+void delete_at_head(Node **head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    Node *temp = *head;
+    *head = (*head)->next;
+
+    free(temp);
+}
+
+void delete_at_tail(Node **head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+
+    Node *current = *head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    free(current->next);
+
+    current->next = NULL;
+}
+
+void delete_at_index(Node **head, int index) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    if (index == 0) {
+        delete_at_head(head);
+        return;
+    }
+
+    Node *current = *head;
+    for (int i = 0; i < index - 1 && current->next != NULL; i++) {
+        current = current->next;
+    }
+
+    if (current->next == NULL) {
+        printf("Index out of bounds\n");
+        return;
+    }
+
+    Node *temp = current->next;
+    current->next = temp->next;
+
+    free(temp);
+}
+
+int main() {
+    Node *head = NULL;
+
+    insert_at_head(&head, 10); // HEAD -> [10] -> NULL
+    insert_at_head(&head, 20); // HEAD -> [20] -> [10] -> NULL
+    insert_at_head(&head, 30); // HEAD -> [30] -> [20] -> [10] -> NULL
+
+    print_list(head);
+
+    insert_at_tail(&head, 40); // HEAD -> [30] -> [20] -> [10] -> [40] -> NULL
+    print_list(head);
+
+    insert_at_index(&head, 50, 2); // HEAD -> [30] -> [20] -> [50] -> [10] -> [40] -> NULL
+    print_list(head);
+
+    delete_at_head(&head); // HEAD -> [50] -> [20] -> [10] -> [40] -> NULL
+    print_list(head);
+
+    delete_at_tail(&head); // HEAD -> [50] -> [20] -> [10] -> NULL
+    print_list(head);
+
+    delete_at_index(&head, 1); // HEAD -> [50] -> [10] -> NULL
+    print_list(head);
+
+    return 0;
+}
---
+++ docs/linked-list/code/main.rs
@@ -0,0 +1,205 @@
+#[allow(dead_code)]
+struct Node {
+    data: i32,
+    next: Option<Box<Node>>,
+}
+
+#[allow(dead_code)]
+impl Node {
+    fn new(data: i32) -> Box<Node> {
+        Box::new(Node { data, next: None })
+    }
+
+    fn get(head: &Option<Box<Node>>, index: usize) -> Option<&Node> {
+        let mut current = head;
+        let mut i = 0;
+
+        while let Some(node) = current {
+            if i == index {
+                return Some(node);
+            }
+
+            current = &node.next;
+            i += 1;
+        }
+
+        None
+    }
+
+    fn print_list(head: &Option<Box<Node>>) {
+        let mut current = head;
+
+        print!("HEAD -> ");
+
+        while let Some(node) = current {
+            print!("{}", node.data);
+
+            if node.next.is_some() {
+                print!(" -> ");
+            }
+
+            current = &node.next;
+        }
+
+        println!(" -> NONE");
+    }
+
+    fn traverse_apply<F>(head: &mut Option<Box<Node>>, mut func: F)
+    where
+        F: FnMut(&mut i32),
+    {
+        let mut current = head;
+
+        while let Some(node) = current {
+            func(&mut node.data);
+
+            current = &mut node.next;
+        }
+    }
+
+    fn insert_at_head(head: Option<Box<Node>>, data: i32) -> Option<Box<Node>> {
+        Some(Box::new(Node { data, next: head }))
+    }
+
+    fn insert_at_tail(head: Option<Box<Node>>, data: i32) -> Option<Box<Node>> {
+        match head {
+            None => Some(Node::new(data)),
+            Some(mut node) => {
+                let mut current = &mut node;
+
+                while current.next.is_some() {
+                    current = current.next.as_mut().unwrap();
+                }
+
+                current.next = Some(Node::new(data));
+
+                Some(node)
+            }
+        }
+    }
+
+    fn insert_at_index(head: Option<Box<Node>>, data: i32, index: usize) -> Option<Box<Node>> {
+        match head {
+            None => {
+                if index == 0 {
+                    Some(Node::new(data))
+                } else {
+                    panic!("Index out of bounds");
+                }
+            }
+            Some(mut node) => {
+                if index == 0 {
+                    return Some(Box::new(Node {
+                        data,
+                        next: Some(node),
+                    }));
+                }
+
+                let mut current = &mut node;
+                for _ in 0..index - 1 {
+                    if current.next.is_none() {
+                        panic!("Index out of bounds");
+                    }
+                    current = current.next.as_mut().unwrap();
+                }
+
+                let new_node = Box::new(Node {
+                    data,
+                    next: current.next.take(),
+                });
+
+                current.next = Some(new_node);
+
+                Some(node)
+            }
+        }
+    }
+
+    fn delete_at_head(head: Option<Box<Node>>) -> Option<Box<Node>> {
+        match head {
+            None => {
+                println!("List is empty");
+                None
+            }
+            Some(node) => node.next,
+        }
+    }
+
+    fn delete_at_tail(head: Option<Box<Node>>) -> Option<Box<Node>> {
+        match head {
+            None => {
+                println!("List is empty");
+                None
+            }
+            Some(mut node) => {
+                if node.next.is_none() {
+                    return Node::delete_at_head(Some(node));
+                }
+
+                let mut current = &mut node;
+                while current.next.as_ref().unwrap().next.is_some() {
+                    current = current.next.as_mut().unwrap();
+                }
+                current.next = None;
+
+                Some(node)
+            }
+        }
+    }
+
+    fn delete_at_index(head: Option<Box<Node>>, index: usize) -> Option<Box<Node>> {
+        match head {
+            None => {
+                println!("List is empty");
+                None
+            }
+            Some(mut node) => {
+                if index == 0 {
+                    return Node::delete_at_head(Some(node));
+                }
+
+                let mut current = &mut node;
+                for _ in 0..index - 1 {
+                    if current.next.is_none() {
+                        panic!("Index out of bounds");
+                    }
+                    current = current.next.as_mut().unwrap();
+                }
+
+                if current.next.is_none() {
+                    panic!("Index out of bounds");
+                }
+
+                let target = current.next.take();
+                current.next = target.unwrap().next;
+
+                Some(node)
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut head = None;
+
+    head = Node::insert_at_head(head, 10); // HEAD -> [10] -> NONE
+    head = Node::insert_at_head(head, 20); // HEAD -> [20] -> [10] -> NONE
+    head = Node::insert_at_head(head, 30); // HEAD -> [30] -> [20] -> [10] -> NONE
+
+    Node::print_list(&head);
+
+    head = Node::insert_at_tail(head, 40); // HEAD -> [30] -> [20] -> [10] -> [40] -> NONE
+    Node::print_list(&head);
+
+    head = Node::insert_at_index(head, 50, 2); // HEAD -> [30] -> [20] -> [50] -> [10] -> [40] -> NONE
+    Node::print_list(&head);
+
+    head = Node::delete_at_head(head); // HEAD -> [50] -> [20] -> [10] -> [40] -> NONE
+    Node::print_list(&head);
+
+    head = Node::delete_at_tail(head); // HEAD -> [50] -> [20] -> [10] -> NONE
+    Node::print_list(&head);
+
+    head = Node::delete_at_index(head, 1); // HEAD -> [50] -> [10] -> NONE
+    Node::print_list(&head);
+}
